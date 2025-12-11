e3efa2ec76d4b731c00536f164b055f5af21f49f
marto3344 <martotbb.2556@gmail.com>
2025-12-12 00:43:09 +0200

    1 client socket server connection test

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,20 +1,65 @@
 #include<iostream>
 #include<vector>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+#define LOCAL_HOST "127.0.0.1"
+#define SERVER_PORT 3000
+#define BUFF_SIZE 1024
 using namespace std;
+
 int main()
 {
-    int threadsCount, size;
-    cout<<"Enter number of threads:\n";
-    cin>>threadsCount;
-    cout<<"Enter array size:\n";
-    cin>>size;
-    vector<double> arr(size, 0);
-    int a;
-    for (size_t i = 0; i < size; i++)
+
+    int sockfd = 0;
+    char buff[BUFF_SIZE];
+    sockaddr_in serv_addr;
+    ssize_t bytes;
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		cerr<<"\n Error : Could not create socket \n";
+		return 1;
+	}
+    serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(SERVER_PORT);
+
+    if(inet_pton(AF_INET, LOCAL_HOST, &serv_addr.sin_addr)<=0)
+	{
+		cerr<<"\n inet_pton error occured!\n";
+		return -1;
+	}
+
+    if (connect(sockfd, (struct sockaddr *) & serv_addr, sizeof(serv_addr)) < 0)
     {
-        cin>>a;
-        arr[i]= a;
+        cerr<<"\n Error connecting to the server!\n";
+        return -1;
     }
+
+    while (1)
+    {
+        bytes = read(0, buff, sizeof(buff));
+        if(bytes <= 0)
+            break;
+        
+        write(sockfd,buff,bytes);
+        sleep(1);
+    }
+    return 0;
+    // int threadsCount, size;
+    // cout<<"Enter number of threads:\n";
+    // cin>>threadsCount;
+    // cout<<"Enter array size:\n";
+    // cin>>size;
+    // vector<double> arr(size, 0);
+    // int a;
+    // for (size_t i = 0; i < size; i++)
+    // {
+    //     cin>>a;
+    //     arr[i]= a;
+    // }
     
 }   
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,74 @@
 #include<iostream>
-#include "merge-sort.h"
+#include <vector>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include <netinet/in.h>
+//#include "merge-sort.h"
+
+
+#define PORT 3000
+#define BUFF_SIZE 1024
+
+void handleclient()
+{
+    
+}
+
+void start()
+{
+
+}
 
 int main(){
-    return 0;
+    sockaddr_in serv_addr;
+    int listenfd = 0, connfd = 0;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    char buffer[BUFF_SIZE];
+    ssize_t bytes;
+
+    if(listenfd == -1)
+    {
+        std::cerr<<"Cannot create socket!\n";
+        return -1;
+    }
     
+    serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(PORT);
+
+    if (bind(listenfd,(sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cerr<<"Error binding!";
+        return - 1;
+    }
+    if (listen(listenfd, 10) < 0)
+    {
+        std::cerr<<"Listen error!";
+        return -1;
+    };
+    std::cout<<"Started listening on port: "<<PORT<<std::endl;
+    connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+    while(1)
+	{
+        
+		bytes = read(connfd, buffer, sizeof(buffer) - 1);
+        if(bytes > 0)
+        {
+            buffer[bytes] ='\0';
+            std::cout<<"Recieved from client:\n";
+            std::cout<<buffer;
+        }
+        else if (bytes == 0)
+        {
+            std::cout<<"CLient closed connection!\n";
+            break;
+        }
+        else{
+            std::cerr<<"Error!\n";
+            break;
+        }
+		sleep(1);
+	}
+    close(connfd);
 }
