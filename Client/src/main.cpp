8ea1237e0f2c428451b5081b6b412b83b832b747
marto3344 <martotbb.2556@gmail.com>
2025-12-14 18:01:55 +0200

    client side logic

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,21 +1,18 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<fstream>
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define LOCAL_HOST "127.0.0.1"
-#define SERVER_PORT 3000
-#define BUFF_SIZE 1024
 using namespace std;
 
-int main()
+int connectToServer(const char* addr, int port)
 {
-
     int sockfd = 0;
-    char buff[BUFF_SIZE];
     sockaddr_in serv_addr;
     ssize_t bytes;
 
@@ -25,9 +22,9 @@ int main()
 		return 1;
 	}
     serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(SERVER_PORT);
+	serv_addr.sin_port = htons(port);
 
-    if(inet_pton(AF_INET, LOCAL_HOST, &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, addr, &serv_addr.sin_addr)<=0)
 	{
 		cerr<<"\n inet_pton error occured!\n";
 		return -1;
@@ -38,28 +35,118 @@ int main()
         cerr<<"\n Error connecting to the server!\n";
         return -1;
     }
+    return sockfd;
+}
 
-    while (1)
+int sendData(int sockfd, const void* data, size_t dataSize)
+{
+    const char * ptr = static_cast<const char*>(data);
+    size_t bytes_sent = 0;
+    while (bytes_sent < dataSize)
     {
-        bytes = read(0, buff, sizeof(buff));
-        if(bytes <= 0)
-            break;
-        
-        write(sockfd,buff,bytes);
-        sleep(1);
+        ssize_t bytes = send(sockfd, ptr + bytes_sent, dataSize - bytes_sent, 0); 
+        if (bytes == -1) {
+            perror("[Client] Send error");
+            return -1;
+        }
+        bytes_sent += bytes;   
     }
     return 0;
-    // int threadsCount, size;
-    // cout<<"Enter number of threads:\n";
-    // cin>>threadsCount;
-    // cout<<"Enter array size:\n";
-    // cin>>size;
-    // vector<double> arr(size, 0);
-    // int a;
-    // for (size_t i = 0; i < size; i++)
-    // {
-    //     cin>>a;
-    //     arr[i]= a;
-    // }
+}
+
+int sendAllInfo(int sockfd, size_t n_threads, const vector<int>& arr)
+{
+    if(sendData(sockfd,&n_threads, sizeof(n_threads))  == -1)
+    {
+        cerr<<"[Client] Failed to send number of threads!";
+        return -1;
+    }
+    const size_t vecsize = arr.size();
+    if(sendData(sockfd,&vecsize, sizeof(vecsize))  == -1)
+    {
+        cerr<<"[Client] Failed to send size of the array";
+        return -1;
+    }
+    cout<<"Sending array!";
+    if(sendData(sockfd, arr.data(), arr.size()*sizeof(int))  == -1)
+    {
+        cerr<<"[Client] Failed to send the array";
+        return -1;
+    }
+    return 0;
+}
+
+int ReceiveSortedArr(int sockfd, std::vector<int>& data, size_t arr_size) 
+{
+    size_t total_bytes = arr_size * sizeof(int);
+    ssize_t bytes_received = recv(
+        sockfd, 
+        reinterpret_cast<char*>(data.data()), 
+        total_bytes, 
+        MSG_WAITALL
+    );
+    if (bytes_received != total_bytes) 
+    {
+        std::cerr << "[Client] Error! Expected " << total_bytes 
+                  << " bytes but received " << bytes_received << " (or error).\n";
+        return -1;
+    }
+    return 0;
+}
+
+void inputFromBinFile(const string& fname, vector<int>& arr)
+{
+    ifstream infile(fname, std::ios::binary | std::ios::ate);
+    if (!infile) 
+    {
+        cerr<<"Error opening the file\n";
+        return;
+    }
+    std::streamsize file_size = infile.tellg();
+    infile.seekg(0, std::ios::beg);
+    size_t array_size = file_size / sizeof(int);
+    arr.resize(array_size);
+    infile.read(reinterpret_cast<char*>(arr.data()), file_size);
+}
+
+int main()
+{
+    const char * addr = "127.0.0.1";
+    int port =  3000;
+    size_t nThreads;
+    cout<<"Enter the number of threads!\n";
+    cin>>nThreads;
+    int arr_input_option = -1;
+    cout<<"How you want to input the array?\n";
+    cout<<"Press 0 to input from binary file \n";
+    cout<<"Press 1 to input from the standart input\n";
+    cin>>arr_input_option;
     
+    vector<int> arr;
+    if(arr_input_option == 0)
+    {   
+        string fname;
+        cout<<"Enter binary file name:\n";
+        cin>>fname;
+        inputFromBinFile(fname, arr);
+    }
+    else if (arr_input_option == 1)
+    {
+        
+    }
+    else
+    {
+        cerr<<"Invalid input option!\n";
+        return 1;
+    }
+    int sockfd = connectToServer(addr, port);
+    if(sockfd == -1)
+    {
+        return 1;
+    }
+    sendAllInfo(sockfd,nThreads, arr);
+    cout<<"Info sent successfully!";  
+    ReceiveSortedArr(sockfd,arr, arr.size());  
+    for (auto val : arr)
+        cout<<val<<' ';
 }   
